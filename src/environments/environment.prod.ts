8d73a5b69989e86878f4dd6f504de8bc8d85ba13
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2020-02-10 11:28:16 +0800

    Added Material and Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyCk_apkb_PUyVcfLpbPGEZ4YO-zyH_p-J8',
+    authDomain: 'angular-fireship-tutoria-53b83.firebaseapp.com',
+    databaseURL: 'https://angular-fireship-tutoria-53b83.firebaseio.com',
+    projectId: 'angular-fireship-tutoria-53b83',
+    storageBucket: 'angular-fireship-tutoria-53b83.appspot.com',
+    messagingSenderId: '250453233680',
+    appId: '1:250453233680:web:e69ed2e6ec8a86419e7a95',
+    measurementId: 'G-576L90750V'
+  }
 };
