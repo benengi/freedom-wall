bf9d58bcc542b61dab10c69d23e25b8f5ce6f2ff
fjgallard <fjdgallardo@gmail.com>
2020-02-11 16:56:58 +0800

    Added User Component

--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { UserRoutingModule } from './user-routing.module';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfilePageComponent, LoginPageComponent],
   imports: [
     CommonModule,
     SharedModule,
